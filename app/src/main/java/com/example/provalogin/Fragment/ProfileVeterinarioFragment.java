5492de9112a762db722cf233c22a2664017fa8be
Lookma96 <Stefanomansi165@gmail.com>
2023-05-13 11:18:26 +0200

    Layout Profilo Veterinario/ Scrittura database fatta , manca solo lettura databse

--- app/src/main/java/com/example/provalogin/Fragment/ProfileVeterinarioFragment.java
+++ app/src/main/java/com/example/provalogin/Fragment/ProfileVeterinarioFragment.java
@@ -1,5 +1,6 @@
 package com.example.provalogin.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,60 +8,51 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.provalogin.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileVeterinarioFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class ProfileVeterinarioFragment extends Fragment {
+public class  ProfileVeterinarioFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ProfileVeterinarioFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileVeterinarioFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProfileVeterinarioFragment newInstance(String param1, String param2) {
-        ProfileVeterinarioFragment fragment = new ProfileVeterinarioFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    TextView profileNameVeterinario, profileEmailVeterinario, profileUsernameVeterinario,profilePasswordVeterinario;
+    TextView titleName, titleUsername;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_veterinario, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile_veterinario, container, false);
+        profileNameVeterinario = view.findViewById(R.id.profileName);
+        profileEmailVeterinario = view.findViewById(R.id.profileEmail);
+        profileUsernameVeterinario = view.findViewById(R.id.profileUsername);
+        profilePasswordVeterinario = view.findViewById(R.id.profilePassword);
+        titleName = view.findViewById(R.id.titleName);
+        titleUsername = view.findViewById(R.id.titleUsername);
+
+        showUserData();
+        return view;
+    }
+
+    public void showUserData(){
+        Intent intent = getActivity().getIntent();
+
+        String nameUser = intent.getStringExtra("Name");
+        String emailUser = intent.getStringExtra("email");
+        String usernameUser = intent.getStringExtra("username");
+        String passwordUser = intent.getStringExtra("password");
+
+        titleName.setText(nameUser);
+        titleUsername.setText(usernameUser);
+        profileNameVeterinario.setText(nameUser);
+        profileEmailVeterinario.setText(emailUser);
+        profileUsernameVeterinario.setText(usernameUser);
+        profilePasswordVeterinario.setText(passwordUser);
     }
 }
